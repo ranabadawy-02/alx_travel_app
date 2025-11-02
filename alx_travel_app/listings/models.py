a96e9f6b9da18608d4d09b241110f8c50cda201f
ranabadawy-02 <ranamohsen1102@gmail.com>
2025-11-02 13:49:02 +0200

    Delete alx_travel_app directory

--- alx_travel_app/listings/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-# Example model (can be expanded later)
-class Listing(models.Model):
-    name = models.CharField(max_length=255)
-    description = models.TextField(blank=True)
-
-    def __str__(self):
-        return self.name
